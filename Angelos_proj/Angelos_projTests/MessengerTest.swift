934a3779d36c3198fc4be120cd81902174c63ba5
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-27 11:18:46 +0100

    Messenger test setup

---
+++ Angelos_proj/Angelos_projTests/MessengerTest.swift
@@ -0,0 +1,24 @@
+//
+//  MessengerTest.swift
+//  Angelos_projTests
+//
+//  Created by Dione Twinkle Estabillo on 27/06/2018.
+//  Copyright © 2018 Habin Kim. All rights reserved.
+//
+
+import XCTest
+@testable import Messenger
+
+class MessengerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+}
