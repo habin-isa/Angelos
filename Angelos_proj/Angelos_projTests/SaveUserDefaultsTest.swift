e66a2ea8a0d514c961b58ec7753ec9594ae5e31c
Charly Mannion <charlottemannion100@gmail.com>
2018-06-28 12:00:56 +0100

    Add a test file for Save User Defaults using Quick and Nimble

---
+++ Angelos_proj/Angelos_projTests/SaveUserDefaultsTest.swift
@@ -0,0 +1,20 @@
+//
+//  SaveUserDefaultsTest.swift
+//  Angelos_projTests
+//
+//  Created by Charly Mannion on 28/06/2018.
+//  Copyright © 2018 Habin Kim. All rights reserved.
+//
+
+import XCTest
+
+import Quick
+import Nimble
+
+@testable import Angelos_proj
+
+class SaveUserDefaultsTest: QuickSpec {
+    override func spec() {
+        super.setUp()
+    }
+}
