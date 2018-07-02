375f07edf1b5319f1e8372e1ea456a59805d86ad
Charly Mannion <charlottemannion100@gmail.com>
2018-07-02 17:41:45 +0100

    creates datetime class

---
+++ Angelos_proj/Angelos_proj/DateTime.swift
@@ -0,0 +1,11 @@
+//
+//  DateTime.swift
+//  Angelos_proj
+//
+//  Created by Charly Mannion on 02/07/2018.
+//  Copyright © 2018 Habin Kim. All rights reserved.
+//
+
+import Foundation
+
+class DateTime {}
