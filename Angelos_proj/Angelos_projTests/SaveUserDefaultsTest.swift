8166e8c618a57683464b37ff4b7f492aedfcfb3b
Charly Mannion <charlottemannion100@gmail.com>
2018-07-02 15:29:32 +0100

    Adds test for a custom message user default

--- Angelos_proj/Angelos_projTests/SaveUserDefaultsTest.swift
+++ Angelos_proj/Angelos_projTests/SaveUserDefaultsTest.swift
@@ -72,6 +72,12 @@ class SaveUserDefaultsTest: QuickSpec {
                     expect(subject.getNumber3()).to(equal("+447718978668"))
                 }
             }
+            describe("#setCustomMessage") {
+                it("sets the custom message attribute") {
+                    subject.setCustomMessage(name: "This is a custom message -")
+                    expect(subject.defaults.string(forKey: "customMessageKey")).to(equal("This is a custom message -"))
+                }
+            }
         }
     }
 }
