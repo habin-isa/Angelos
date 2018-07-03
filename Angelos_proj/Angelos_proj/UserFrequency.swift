b43e7e6da5dbfe636d1b2b6d6579c702893b8a5b
Charly Mannion <charlottemannion100@gmail.com>
2018-07-03 12:27:47 +0100

    Tests for a user frequency class with default value of clicks, implements class

---
+++ Angelos_proj/Angelos_proj/UserFrequency.swift
@@ -0,0 +1,13 @@
+//
+//  UserFrequency.swift
+//  Angelos_proj
+//
+//  Created by Charly Mannion on 03/07/2018.
+//  Copyright © 2018 Habin Kim. All rights reserved.
+//
+
+import Foundation
+
+class UserFrequency {
+    var clicks = 0
+}
---
+++ Angelos_proj/Angelos_projTests/UserFrequencyTest.swift
@@ -0,0 +1,30 @@
+//
+//  UserFrequencyTest.swift
+//  Angelos_projTests
+//
+//  Created by Charly Mannion on 03/07/2018.
+//  Copyright © 2018 Habin Kim. All rights reserved.
+//
+
+import XCTest
+@testable import Angelos_proj
+
+class UserFrequencyTest: XCTestCase {
+    
+    let userFrequency = UserFrequency()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testDefaultOfNoUsage() {
+        XCTAssertEqual(0, userFrequency.clicks)
+    }
+    
+}
