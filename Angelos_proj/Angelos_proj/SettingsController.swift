24c56e80fe53508f0682600805a75c7354082482
Habin <habin@hotmail.co.uk>
2018-07-04 16:16:00 +0100

    all logic extracted from original ViewController

--- Angelos_proj/Angelos_proj/SettingsController.swift
+++ Angelos_proj/Angelos_proj/SettingsController.swift
@@ -10,6 +10,13 @@ import Foundation
 import UIKit
 
 class SettingsController: UIViewController {
+    
+    var name = ""
+    var number1 = ""
+    var number2 = ""
+    var number3 = ""
+    var customMessage = ""
+    var savedDefault = SaveUserDefaults()
   
     @IBOutlet weak var nameOutputField: UILabel!
     @IBOutlet weak var nameInputField: UITextField!
@@ -19,11 +26,51 @@ class SettingsController: UIViewController {
     @IBOutlet weak var numberOutputField1: UILabel!
     @IBOutlet weak var numberOutputField2: UILabel!
     @IBOutlet weak var numberOutputField3: UILabel!
-
+    @IBOutlet weak var customMsgInputField: UITextField!
+    @IBOutlet weak var customMsgOutputField: UILabel!
     
-    func viewDidLoad() {
+    override func viewDidLoad() {
+        name = savedDefault.getName()
+        number1 = savedDefault.getNumber1()
+        number2 = savedDefault.getNumber2()
+        number3 = savedDefault.getNumber3()
+        customMessage = savedDefault.getCustomMessage()
         nameOutputField.text = "\(name)"
+        numberOutputField1.text = "\(number1)"
+        numberOutputField2.text = "\(number2)"
+        numberOutputField3.text = "\(number3)"
+        customMsgOutputField.text = "\(customMessage)"
     }
     
+    @IBAction func clickSave(_ sender: UIButton) {
+        customMessage = customMsgInputField.text!
+        customMsgInputField.text = ""
+        customMsgOutputField.text = "\(customMessage)"
+        savedDefault.setCustomMessage(customMessage: customMessage)
+    }
+    
+    @IBAction func clickSubmit(_ sender: Any) -> Void {
+        name = nameInputField.text!
+        number1 = numberInputField1.text!
+        number2 = numberInputField2.text!
+        number3 = numberInputField3.text!
+        numberInputField1.text = ""
+        numberInputField2.text = ""
+        numberInputField3.text = ""
+        nameInputField.text = ""
+        
+        numberOutputField1.text = "\(number1)"
+        numberOutputField2.text = "\(number2)"
+        numberOutputField3.text = "\(number3)"
+        nameOutputField.text = "\(name)"
+        
+        savedDefault.setName(name: name)
+        savedDefault.setNumber1(number: number1)
+        savedDefault.setNumber2(number: number2)
+        savedDefault.setNumber3(number: number3)
+        Messenger().sendMessage(phoneNumber: number1, type: "inform", userName: name)
+        Messenger().sendMessage(phoneNumber: number2, type: "inform", userName: name)
+        Messenger().sendMessage(phoneNumber: number3, type: "inform", userName: name)
+    }
     
 }
--- Angelos_proj/Angelos_proj/ViewController.swift
+++ Angelos_proj/Angelos_proj/ViewController.swift
@@ -13,32 +13,21 @@ import CoreLocation
 class ViewController: UIViewController{
         
     var ringtonePlayer = AudioPlayer()
-    
-    var savedDefault = SaveUserDefaults()
-    var frequencyDefault = UserFrequency()
     var name = ""
     var number1 = ""
     var number2 = ""
     var number3 = ""
     var customMessage = ""
+    var savedDefault = SaveUserDefaults()
+    var frequencyDefault = UserFrequency()
     var userFrequency = 0
     
     @IBOutlet weak var timeInputField: UITextField!
-    @IBOutlet weak var customMsgInputField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         warningOutputField.isHidden = true
-        name = savedDefault.getName()
-        number1 = savedDefault.getNumber1()
-        number2 = savedDefault.getNumber2()
-        number3 = savedDefault.getNumber3()
-        customMessage = savedDefault.getCustomMessage()
         userFrequency = frequencyDefault.getUserFrequency()
-        numberOutputField1.text = "\(number1)"
-        numberOutputField2.text = "\(number2)"
-        numberOutputField3.text = "\(number3)"
-        customMsgOutputField.text = "\(customMessage)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -84,37 +73,6 @@ class ViewController: UIViewController{
     }
 
     
-    @IBAction func clickSubmit(_ sender: Any) -> Void {
-        name = nameInputField.text!
-        number1 = numberInputField1.text!
-        number2 = numberInputField2.text!
-        number3 = numberInputField3.text!
-        numberInputField1.text = ""
-        numberInputField2.text = ""
-        numberInputField3.text = ""
-        nameInputField.text = ""
-        
-        numberOutputField1.text = "\(number1)"
-        numberOutputField2.text = "\(number2)"
-        numberOutputField3.text = "\(number3)"
-        nameOutputField.text = "\(name)"
-        
-        savedDefault.setName(name: name)
-        savedDefault.setNumber1(number: number1)
-        savedDefault.setNumber2(number: number2)
-        savedDefault.setNumber3(number: number3)
-        Messenger().sendMessage(phoneNumber: number1, type: "inform", userName: name)
-        Messenger().sendMessage(phoneNumber: number2, type: "inform", userName: name)
-        Messenger().sendMessage(phoneNumber: number3, type: "inform", userName: name)
-    }
-    
-    @IBAction func clickSave(_ sender: UIButton) {
-        customMessage = customMsgInputField.text!
-        customMsgInputField.text = ""
-        customMsgOutputField.text = "\(customMessage)"
-        savedDefault.setCustomMessage(customMessage: customMessage)
-    }
-    
     func showWarning() {
         if frequencyDefault.triggerConcern() == true {
             warningOutputField.isHidden = false
@@ -124,8 +82,6 @@ class ViewController: UIViewController{
     }
     
    
-
-    @IBOutlet weak var customMsgOutputField: UILabel!
     @IBOutlet weak var warningOutputField: UILabel!
     
     
